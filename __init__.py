0e74aa5017f1b295575adc917459bc1beaa0cd92
Will Maier <wcmaier@m.aier.us>
2014-10-13 06:10:08 +0000

    param controls

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 
 import Live
 from _Framework.ButtonElement import ButtonElement
+from _Framework.DeviceComponent import DeviceComponent
 # from _Framework.ButtonMatrixElement import ButtonMatrixElement
 # from _Framework.ChannelStripComponent import ChannelStripComponent
 from _Framework.ControlSurface import OptimizedControlSurface
@@ -63,12 +64,12 @@ class Encoder(EncoderElement):
 
 class Mixer(SpecialMixerComponent):
     tracks = TRACKS
+    name = 'Mixer'
 
     def __init__(self):
         SpecialMixerComponent.__init__(
             self,
             self.tracks)
-        self.name = 'Mixer'
         self.strips()
 
     def strips(self):
@@ -93,12 +94,49 @@ class VolumeFader(Encoder):
         self.name = "Volume_Fader_%d" % index
 
 
+class Device(DeviceComponent):
+    name = 'Device_Component'
+    rows = 2
+    columns = 4
+
+    def __init__(self):
+        DeviceComponent.__init__(
+            self)
+        parameter_controls = list(self.parameter_controls())
+        self.set_parameter_controls(parameter_controls)
+
+    def parameter_controls(self):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                yield ParameterEncoder(row, column)
+
+
+class ParameterEncoder(Encoder):
+    ccs = [
+        # Reserve the top row.
+        # [4,  5,  6,  7],
+        [8,  9,  10, 11],
+        [12, 13, 14, 15],
+    ]
+
+    def __init__(self, row, index):
+        Encoder.__init__(
+            self,
+            self.ccs[row][index])
+        x = index + 1
+        y = row + 1
+        self.parameter = x * y
+        self.name = "Device_Parameter_%d_Control" % self.parameter
+
+
 class Controller(OptimizedControlSurface):
 
     def __init__(self, c):
         OptimizedControlSurface.__init__(self, c)
         with self.component_guard():
             mixer = Mixer()
+            device = Device()
+            self.set_device_component(device)
             # session = self.session()
             # session.set_mixer(mixer)
 
